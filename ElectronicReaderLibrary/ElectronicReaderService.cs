70d4a2decff6c36d3fa756f0d354a91c68312db5
Tatiana <tatyana.getmanova@gmail.com>
2017-05-14 17:02:47 +0300

    Add ReaderService for manage reader instances

---
+++ ElectronicReaderLibrary/ElectronicReaderService.cs
@@ -0,0 +1,17 @@
+﻿namespace ElectronicReaderLibrary
+{
+    /// <summary>
+    /// 'Service' methods to manage electronic reader instances
+    /// </summary>
+    public class ElectronicReaderService
+    {
+        /// <summary>
+        /// Gets the reader.
+        /// </summary>
+        /// <returns></returns>
+        public ElectronicReader GetReader()
+        {
+            return new ElectronicReader();
+        }
+    }
+}
